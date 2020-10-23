52b0112069b8fd761633f9613f6831c9b7e7ec25
matthanes <hanes.matt@gmail.com>
2020-10-23 19:43:50 -0400

    Update DiscussionsToHTML.py
    Formatting changes

--- DiscussionsToHTML/DiscussionsToHTML.py
+++ DiscussionsToHTML/DiscussionsToHTML.py
@@ -1,7 +1,7 @@
 from canvasapi import Canvas
 import csv,sys,json,os
 
-token = os.getenv('CanvasTKN') # Your Canvas API token. Obfuscated here for publishing to GitHub.
+token = 'TokenGoesHere' # Your Canvas API token. Obfuscated here for publishing to GitHub.
 
 # Canvas API URL
 API_URL = "https://mcsd.instructure.com/" # Your Canvas URL here
@@ -13,7 +13,7 @@ canvas = Canvas(API_URL, token)
 course = canvas.get_course('118557') 
 
 # Discussion ID here
-discussion = course.get_discussion_topic('153501') 
+discussion = course.get_discussion_topic('160234') 
 
 # Get the replies for the discussion
 replies = discussion.get_topic_entries()
@@ -26,9 +26,10 @@ with open( 'replies.html', 'w' ) as file:
     file.write( '<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />\n' )
     file.write( '<title>%s</title>\n' % (discussion.title) )
     file.write( '</head>\n' )
+    file.write( '<style>p, ol {font-size: 1.3rem}</style>\n' )
     file.write( '<body>\n' )
     topic = discussion.message.replace('src="//www','src="https://www') #fixes YouTube links
-    file.write( '<h1>%s</h1>\n' % (topic.replace('\xa0',' ')) ) #removes &nbsp and changes them to single spaces.
+    file.write( '<h1 style="max-width: 800px;margin: 20px auto;">%s</h1>\n' % (topic.replace('\xa0',' ')) ) #removes &nbsp and changes them to single spaces.
 
     for reply in replies:
         file.write( '<div style="max-width: 800px; margin: 20px auto; border: 2px solid #888; padding: 20px;">\n')
@@ -39,4 +40,3 @@ with open( 'replies.html', 'w' ) as file:
     file.write( '</html>\n' )
 
 file.close()
-
